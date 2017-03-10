dcb654864925b1991c6268ef28ed72f9e6b9eb63
briholt100 <brian.c.holt@gmail.com>
2017-03-10 11:42:36 -0800

    tweaked dplyr to ggplot

--- leapfrog.r
+++ leapfrog.r
@@ -9,9 +9,14 @@ df$cutYearsWorked<-cut(df$SRV.YEARS,5)
 levels(df$cutYearsWorked)<-c("1-10","10 - 20","20-29","29-38","38-47")
 
 head(df)
-df %>% ggplot(aes(x=SRV.YEARS,y=BASE.SALARY,group=cutYearsWorked,color=cutYearsWorked))+geom_point(size=1.5)+
+
+#the following looks just at people likely to teach lab
+df %>% filter(LAB !='N') %>% select(-cutYearsWorked, -PROG.SYMBOL, -PROGRAM) %>% 
+  mutate(cutYearsWorked = cut(SRV.YEARS, breaks = quantile(SRV.YEARS, probs = seq(0, 1, 0.33)))) %>%
+  ggplot(aes(x=SRV.YEARS,y=BASE.SALARY,group=cutYearsWorked,color=cutYearsWorked))+
+  geom_point(size=1.5)+
   geom_smooth(se = T, method = "lm")+
-  labs(title = "linear model applied to 5 different \ncohorts separated by years worked\nShaded region is Standard Error",
+  labs(title = "linear model applied to 5 different lab teaching \ncohorts separated by years worked\nShaded region is Standard Error",
        x = "Service years")
 
 
