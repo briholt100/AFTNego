23b67ba150c462b5a0764f1bbec9bceb764b6f28
briholt100 <brian.c.holt@gmail.com>
2017-03-13 17:40:58 -0700

    ggplot adjustments

--- leapfrog.r
+++ leapfrog.r
@@ -17,13 +17,16 @@ levels(df$cutYearsWorked)<-c("1-10","10 - 20","20-29","29-38","38-47")
 head(df)
 
 #the following looks just at people likely to teach lab
-df %>% filter(LAB !='N') %>% select(-cutYearsWorked, -PROG.SYMBOL, -PROGRAM) %>% 
-  mutate(cutYearsWorked = cut(SRV.YEARS, breaks = quantile(SRV.YEARS, probs = seq(0, 1, 0.33)))) %>%
-  ggplot(aes(x=SRV.YEARS,y=BASE.SALARY,group=cutYearsWorked,color=cutYearsWorked))+
+df %>% 
+#  filter(LAB !='N') %>% 
+  select(-cutYearsWorked, -PROG.SYMBOL, -PROGRAM) %>% 
+  mutate(cutYearsWorked = cut(SRV.YEARS, breaks = quantile(SRV.YEARS, probs = seq(0, 1, 0.21)))) %>%
+#df %>% 
+    ggplot(aes(x=SRV.YEARS,y=BASE.SALARY,group=cutYearsWorked,color=cutYearsWorked))+
   geom_point(size=1.5)+
   geom_smooth(se = T, method = "lm")+
-  labs(title = "linear model applied to 5 different lab teaching \ncohorts separated by years worked\nShaded region is Standard Error",
-       x = "Service years")
+  labs(title = "linear model applied to 5 teaching \ncohorts separated by years worked\nShaded region is Standard Error",
+       x = "Service years\nAll faculty")
 
 
 Lab.df<-read.csv(file="I:/My Data Sources/AFT Data/labLeap.csv",na.strings = "",sep="\t")
