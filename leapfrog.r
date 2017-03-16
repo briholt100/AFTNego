4449591b93b761dcc360d12786982bcbd3c2be89
briholt100 <brian.c.holt@gmail.com>
2017-03-16 12:33:09 -0700

    finding unique items

--- leapfrog.r
+++ leapfrog.r
@@ -13,8 +13,12 @@ str(df)
 range(df$SRV.YEARS)
 df$cutYearsWorked<-cut(df$SRV.YEARS,5,replace=F)
 levels(df$cutYearsWorked)<-c("1-10","10 - 20","20-29","29-38","38-47")
-
 head(df)
+
+
+b<-df %>% select(PROGRAM ,PROG.SYMBOL)
+nrow(unique(b))
+
 
 #the following looks just at people likely to teach lab
 df %>% 
@@ -29,8 +33,9 @@ df %>%
     ggplot(aes(x=SRV.YEARS,y=BASE.SALARY,group=cutYearsWorked,color=cutYearsWorked))+
   geom_point(size=1.5)+
   geom_smooth(se = T, method = "lm")+
-  labs(title = "linear model applied to 5 teaching \ncohorts separated by years worked\nShaded region is Standard Error",
-       x = "Service years\nLAb faculty")
+  labs(title = "linear model applied to 5 teaching \ncohorts separated by years worked\n\nGrids are weekly workload",
+       x = "Service years")+
+facet_wrap(~WEEKLY.LOAD)
 
 Lab.df<-read.csv(file="I:/My Data Sources/AFT Data/labLeap.csv",na.strings = "",sep="\t")
 Lab.df$cutYearsWorked<-cut(Lab.df$yrs,5)
